ec7e04d8dea951fc8d04f49b84e1e70c24b13112
Ghufran <ghufranshahid101@gmail.com>
2022-08-23 23:21:54 -0400

    added ability to configure grid size and added event listener to change tile colour

--- etch.js
+++ etch.js
@@ -5,10 +5,14 @@ function defaultGrid(gridSize){
 
     for (let i = 0; i < gridSize; i++){
         const div = document.createElement('div');
-        div.classList.add('tile');
         div.style.height = `calc(100vh / ${Math.sqrt(gridSize)})`;
         div.style.width = `calc(100vw / ${Math.sqrt(gridSize)})`;
+        div.addEventListener('pointerenter', function(e){
+            div.classList.add('tile');
+        });
+
         sketchPad.appendChild(div);
     }
+    
 }
-defaultGrid(16);
+defaultGrid(100);
