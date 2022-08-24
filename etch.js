4195b9cb270d25aeb6961d6346fb272486991cc4
Ghufran <ghufranshahid101@gmail.com>
2022-08-23 23:09:00 -0400

    Added calculations for creating tiles that are equal space

--- etch.js
+++ etch.js
@@ -1,12 +1,14 @@
 
 const sketchPad = document.querySelector('.sketchPad');
 
-function defaultGrid(){
+function defaultGrid(gridSize){
 
-    for (let i = 0; i < 16; i++){
+    for (let i = 0; i < gridSize; i++){
         const div = document.createElement('div');
+        div.classList.add('tile');
+        div.style.height = `calc(100vh / ${Math.sqrt(gridSize)})`;
+        div.style.width = `calc(100vw / ${Math.sqrt(gridSize)})`;
         sketchPad.appendChild(div);
     }
 }
-
-defaultGrid();
+defaultGrid(16);
